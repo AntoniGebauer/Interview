a2589abc71ac0a6dd22d655f0b0a1ecf242f09d1
AntoniGebauer <antoni.gebauer@gmail.com>
2022-07-19 14:14:21 +0200

    Initial Commit

---
+++ Interview/InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewApp.swift
+//  Interview
+//
+//  Created by Antoni Gebauer on 19/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
